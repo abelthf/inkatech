2c3d36ad6b38ba9f5ee58fb2045bd47a903c46b2
Fredy Abel Huanca Torres <abel.huanca@upeu.edu.pe>
2018-11-23 14:16:52 -0500

    configurando docker

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,14 +1,21 @@
 # services/users/project/__init__.py
 
-
+import os                                   #nuevo
 from flask import Flask, jsonify
 
 
 # Istanciado la app
 app = Flask(__name__)
 
+# estableciendo configuracion
+app_settings = os.getenv('APP_SETTINGS')    #nuevo
+app.config.from_object(app_settings)        #nuevo
+
 # Establecer configuracion
 app.config.from_object('project.config.DevelopmentConfig')
+
+import sys
+print(app.config, file=sys.stderr)
 
 @app.route('/users/ping', methods=['GET'])
 def ping_pong():
